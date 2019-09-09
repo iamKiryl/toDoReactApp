90886cae8da39a841edb2c6109fa73b7e5e04a87
Kiryl Matusevich <iamkirylm@gmail.com>
2019-09-09 22:17:55 +0200

    Updates

---
+++ precache-manifest.c377f2e851e5dd6e7c0cb2594c24c5c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c18f3090a497652333fd35642df404c",
+    "url": "/toDoReactApp/index.html"
+  },
+  {
+    "revision": "1b3a0092a7f29684a51a",
+    "url": "/toDoReactApp/static/css/main.48b05b5b.chunk.css"
+  },
+  {
+    "revision": "9319774a87fb395a0206",
+    "url": "/toDoReactApp/static/js/2.d6ab339c.chunk.js"
+  },
+  {
+    "revision": "1b3a0092a7f29684a51a",
+    "url": "/toDoReactApp/static/js/main.e0fc4355.chunk.js"
+  },
+  {
+    "revision": "1932b6ffd58fbaf8b01a",
+    "url": "/toDoReactApp/static/js/runtime~main.766efa69.js"
+  }
+]);
